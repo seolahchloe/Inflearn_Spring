b0135d86f61bd6307362920a3be63d36b61cf54a
chloe <seolahchloe.chung@gmail.com>
2020-12-30 23:54:53 +0900

    Update Controllers, Repository and MemberServiceTest

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -19,7 +19,7 @@ public class HelloController {
     @GetMapping("hello-mvc")
     public String helloMvc(@RequestParam("name") String name, Model model) {
         model.addAttribute("name", name);
-        return "hello-templete";
+        return "hello-template";
     }
 
     // API
