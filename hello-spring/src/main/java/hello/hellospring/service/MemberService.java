83ced592dfbbe443138a7bd4522db1378de584e8
chloe <seolahchloe.chung@gmail.com>
2020-12-30 23:45:18 +0900

    Add new sql file

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -3,13 +3,17 @@ package hello.hellospring.serive;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+
 public class MemberService {
 
     private final MemberRepository memberRepository;
+
 
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;  // cmd + n
@@ -18,7 +22,7 @@ public class MemberService {
     /**
      * 회원 가입
      */
-    
+
     public Long join(Member member) {
         // 같은 이름이 있는 중복 회원X
         validateDuplicateMember(member);  // 중복 회원 검증
